40130ac0261f6d255a7c96bfaef618a1db3a6daf
jt <jt1xx9@gmail.com>
2024-06-30 11:07:45 +0700

    add: Add base structure api for university project

---
+++ server/src/routes/api.route.ts
@@ -0,0 +1,8 @@
+import { Application } from "express";
+
+import { notFoundHandlerMiddleware } from "../middlewares";
+
+export const apiRoute = (app: Application) => {
+
+    app.use("*", notFoundHandlerMiddleware);
+}
